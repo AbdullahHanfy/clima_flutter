caea2069f86d7fbc646ac6de7770977e1e1a3398
Abdullah Hanfy <abdallahhanfy@outlook.com>
2020-08-23 20:21:08 +0200

    final

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -3,7 +3,7 @@ import 'package:geolocator/geolocator.dart';
 class Location {
   double latitude;
   double longtude;
-
+  //Function to fit location of your device
   Future<void> getLocation() async {
     try {
       Position position = await Geolocator().getCurrentPosition(
